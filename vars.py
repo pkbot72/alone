3f5f3955d1f5ea2db49e48406c6e2ccb36865846
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2024-04-16 00:33:43 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
 api_id = "23881274"
 api_hash = "c867262073a58ccdebcae19601c5bf5f"
 bot_token = "7173171084:AAEOkdr8x7LFW0Q31GdNvGzeMt2gDA1g3YY"
-auth_groups = "-7156412102"
+auth_groups = "-1002040684965"
