068f72424ddc7fe88a07e1b5a91586e0e3a2ae17
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2024-04-16 00:21:16 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-api_id = "25284113"
-api_hash = "a17e15f84e6cabf51a0cce7fe4e88f7c"
-bot_token = "7084779632:AAGXQ5xf74pCGCzZU5AUtuN0lJF_VJwnlbo"
-auth_groups = "-1002038669257"
+api_id = "23881274"
+api_hash = "c867262073a58ccdebcae19601c5bf5f"
+bot_token = "7173171084:AAEOkdr8x7LFW0Q31GdNvGzeMt2gDA1g3YY"
+auth_groups = "-7156412102"
