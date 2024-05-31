3978b6e322af1c56c92006defa1a22ae7e5b2eed
pkbot72 <101813111+pkbot72@users.noreply.github.com>
2024-05-31 22:37:39 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-api_id = "26395620"
-api_hash = "906cc7e4b4c1ad7f7121efc2244d13ad"
-bot_token = "6864254099:AAGC3zQAbJutJKks7-dLksInj6YL9GdR5dU"
-auth_groups = "-1001928293542"
+api_id = "9741228"
+api_hash = "6f339e8fa827a5147fa2ff03898d610c"
+bot_token = "6375464387:AAHTEpD_U--E9AzvbeGzxzuBKhceQIM6rHw"
+auth_groups = "-1001886332246"
